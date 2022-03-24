d3087373d6c093c44752c8002465188ac95675dc
Berestov <berestov@protei.ru>
2022-03-24 17:58:34 +0300

    Fix project

--- src/test/java/test/TestPatterns.java
+++ src/test/java/test/TestPatterns.java
@@ -7,44 +7,44 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TestPatterns{
+public class TestPatterns {
 
-        @BeforeAll
-        static void setUpAll() {
-            SelenideLogger.addListener("allure", new AllureSelenide());
-        }
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
-        @AfterAll
-        static void tearDownAll() {
-            SelenideLogger.removeListener("allure");
-        }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
-        @BeforeEach
-        void setUp() {
-            open("http://localhost:9999/");
-        }
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+    }
 
-        @Test
-        void shouldSendCorrectForm() {
-            Data.UserInfo user = Data.Registration.generateUser();
-            $("[data-test-id='city'] input").setValue(Data.getRandomCity());
-            $("[data-test-id=date] input").doubleClick().sendKeys(Data.generateDate(3));
-            $("[data-test-id=name] input").setValue(user.getName());
-            $("[data-test-id=phone] input").setValue(user.getPhone());
-            $(".checkbox__box").click();
-            $$("button").find(exactText("Запланировать")).click();
-            $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + Data.generateDate(3)), Duration.ofSeconds(15));
-            $("[data-test-id=date] input").doubleClick().sendKeys(Data.generateDate(6));
-            $$("button").find(exactText("Запланировать")).click();
-            $("[data-test-id=replan-notification]").shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15));
-            $$("button").find(exactText("Перепланировать")).click();
-            $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + Data.generateDate(6)), Duration.ofSeconds(15));
-        }
+    @Test
+    void shouldSendCorrectForm() {
+        Data.UserInfo user = Data.Registration.generateUser();
+        $("[data-test-id='city'] input").setValue(Data.getRandomCity());
+        $("[data-test-id=date] input").doubleClick().sendKeys(Data.generateDate(3));
+        $("[data-test-id=name] input").setValue(user.getName());
+        $("[data-test-id=phone] input").setValue(user.getPhone());
+        $(".checkbox__box").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + Data.generateDate(3)), Duration.ofSeconds(15));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Data.generateDate(6));
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=replan-notification]").shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15));
+        $$("button").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + Data.generateDate(6)), Duration.ofSeconds(15));
     }
 }
 
